ccf2de2c7f2a1d0f6e6aa143b1f5965548b291e5
Thomas Grant <thomas.grant.duck321@gmail.com>
2022-02-24 18:35:24 +0000

    Struct print debugging facilities

--- structs.py
+++ structs.py
@@ -8,13 +8,18 @@ class Skill:
 
 class Person:
     def __init__(self, name, skills: list) -> None:
+        self.assigned_tasks = []
         self.name = name
         self.skills = {}
         for skill in skills:
             self.skills[skill.name] = skill
+    
+    def __repr__(self) -> str:
+        return f"Person({self.name})"
 
 class Task:
-    def __init__(self, required_skills: list, due_date: int, duration: int) -> None:
+    def __init__(self, name: str, required_skills: list, due_date: int, duration: int) -> None:
+        self.name = name
         self.required_skills = {}
         for skill in required_skills:
             self.required_skills[skill.name] = skill
@@ -22,3 +27,6 @@ class Task:
         self.due_date = due_date
         self.duration = duration
         self.latest_start_date = due_date - duration
+
+    def __repr__(self) -> str:
+        return f"Task({self.name}, due: {self.due_date})"
