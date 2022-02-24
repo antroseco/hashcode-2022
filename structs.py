0c08bdcec5a19ade32679fb64f78d35cbf41db09
Andreas Economides <antroseconomides@hotmail.co.uk>
2022-02-24 18:44:58 +0000

    Add Score to the Tasks struct

--- structs.py
+++ structs.py
@@ -2,9 +2,10 @@ class Skill:
     def __init__(self, name: str, level: int) -> None:
         self.name = name
         self.level = level
-    
+
     def __repr__(self) -> str:
         return f"Skill({self.name}, {self.level})"
+
 
 class Person:
     def __init__(self, name, skills: list) -> None:
@@ -13,12 +14,13 @@ class Person:
         self.skills = {}
         for skill in skills:
             self.skills[skill.name] = skill
-    
+
     def __repr__(self) -> str:
         return f"Person({self.name})"
 
+
 class Task:
-    def __init__(self, name: str, required_skills: list, due_date: int, duration: int) -> None:
+    def __init__(self, name: str, required_skills: list, due_date: int, duration: int, score: int) -> None:
         self.name = name
         self.required_skills = {}
         for skill in required_skills:
@@ -26,6 +28,7 @@ class Task:
 
         self.due_date = due_date
         self.duration = duration
+        self.score = score
         self.latest_start_date = due_date - duration
 
     def __repr__(self) -> str:
